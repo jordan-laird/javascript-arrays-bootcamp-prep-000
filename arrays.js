53cfa29df750678552829f289d6fd8163ec7ce98
Jordan Laird <jordanmack.laird@gmail.com>
2018-09-25 16:24:18 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,12 @@
+var chocolateBars = [
+  'snickers',
+  'hundred grand',
+  'kitkat',
+  'skittles'
+  ]
+  
+function addElementToBeginningOfArray(array,element){
+ [element, ...array]
+ return array
+}
+
